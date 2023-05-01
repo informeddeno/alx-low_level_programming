482d530c22c1e4a6c83c8e326802b08589a4513b
informeddeno <informeddeno@gmail.com>
2023-05-01 12:17:55 -0700

    linked list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * listint_len - returns number of elements in linked list
+ * @h: linked list of type listint_t
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+
+{
+size_t item = 0;
+while (h)
+{
+item++;
+h = h->next;
+}
+return (item);
+}
