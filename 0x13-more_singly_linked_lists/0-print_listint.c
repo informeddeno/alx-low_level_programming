222de93079ec2b056d05869c70e3afbd37050c91
informeddeno <informeddeno@gmail.com>
2023-05-03 10:43:53 -0700

    print elements of linked list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,6 +4,7 @@
  * @h: linked list of type listint_t to print
  * Return: number of nodes
  */
+
 size_t print_listint(const listint_t *h)
 {
   size_t item = 0;
@@ -14,5 +15,6 @@ size_t print_listint(const listint_t *h)
       item++;
       h = h->next;
     }
+
   return (item);
 }
