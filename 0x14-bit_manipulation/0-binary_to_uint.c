7582f9e744c4a2d55427c0cc4aa3ddbed0f5b963
informeddeno <informeddeno@gmail.com>
2023-07-07 08:02:06 -0700

    result

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,30 +2,33 @@
 #include <stdlib.h>
 #include "main.h"
 
-unsigned int binary_to_uint(const char *b);
-
-/**
- * binary_to_uint - converts a binary number to an unsigned int.
+/* binary_to_uint - converts a binary number to an unsigned int.
  * @b: string containing the binary number.
  *
  * Return: the converted number, or 0 if there is one or more chars in the string
  * b that is not 0 or 1, or if b is NULL.
  */
-unsigned int binary_to_uint(const char *b)
+unsigned int binary_to_uint(const char *b);
+
+int main(void)
 {
-    unsigned int convt = 0;
-    int i;
+	const char *binary = "101010"; /* Example binary number */
+	unsigned int convt = 0;
+	unsigned int result = 0;
+	int i;
 
-    if (b == NULL)
-        return 0;
+	if (binary == NULL)
+		return 0;
 
-    for (i = 0; b[i] != '\0'; i++)
-    {
-        if (b[i] != '0' && b[i] != '1')
-            return 0;
+	for (i = 0; binary[i] != '\0'; i++)
+	{
+		if (binary[i] != '0' && binary[i] != '1')
+			return 0;
 
-        convt = 2 * convt + (b[i] - '0');
-    }
+		convt = 2 * convt + (binary[i] - '0');
+	}
 
-    return convt;
+	result = binary_to_uint(binary);
+
+	return 0;
 }
