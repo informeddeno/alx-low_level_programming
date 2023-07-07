76ff85b1e3aa56b1953b80690a35be67a992f8b3
informeddeno <informeddeno@gmail.com>
2023-07-07 09:05:18 -0700

    binary to unit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,22 +6,23 @@ unsigned int binary_to_uint(const char *b);
 
 int main(void)
 {
-	const char *binary = "101010";	/* Example binary number */
-	unsigned int convt = 0;
-	int i;
+    const char *binary = "101010";  /* Example binary number */
+    unsigned int convt = 0;
+    int i;
 
-	if (binary == NULL)
-		return (0);
+    if (binary == NULL) {
+        return 0;
+    }
 
-	for (i = 0; binary[i] != '\0'; i++)
-	{
-		if (binary[i] != '0' && binary[i] != '1')
-			return (0);
+    for (i = 0; binary[i] != '\0'; i++) {
+        if (binary[i] != '0' && binary[i] != '1') {
+            return 0;
+        }
 
-		convt = 2 * convt + (binary[i] - '0');
-	}
+        convt = 2 * convt + (binary[i] - '0');
+    }
 
-	binary_to_uint(binary);
+    binary_to_uint(binary);
 
-	return (0);
+    return 0;
 }
