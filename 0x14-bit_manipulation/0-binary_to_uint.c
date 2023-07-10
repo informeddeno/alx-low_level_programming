4cddecf17cd9bd248dda4c7fc59258f1127a707d
informeddeno <informeddeno@gmail.com>
2023-07-10 02:27:14 -0700

    binary to uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -16,36 +16,35 @@ int _putchar(char c);
 int main(void)
 {
     const char *binary = "101010";  /* Example binary number */
-    unsigned int convt = 0;
-    unsigned int decimal = 0;  /* Move the declaration here */
+    unsigned int decimal = 0;
     int i;
 
     if (binary == NULL)
-        return (0);
+        return 0;
 
     for (i = 0; binary[i] != '\0'; i++)
     {
         if (binary[i] != '0' && binary[i] != '1')
-            return (0);
+            return 0;
 
-        convt = 2 * convt + (binary[i] - '0');
+        decimal = 2 * decimal + (binary[i] - '0');
     }
 
-    decimal = binary_to_uint(binary);  /* Assign the value here */
+    unsigned int result = binary_to_uint(binary);
 
-    if (decimal == 0)
+    if (result == 0)
         _putchar('0');
     else
     {
-        while (decimal > 0)
+        while (result > 0)
         {
-            int remainder = decimal % 10;
+            int remainder = result % 10;
             _putchar(remainder + '0');
-            decimal /= 10;
+            result /= 10;
         }
     }
 
     _putchar('\n');
 
-    return (0);
+    return 0;
 }
