cd6717b426d5d28224a6fef31f69e9f25eee3817
informeddeno <informeddeno@gmail.com>
2023-07-10 05:00:37 -0700

    binary to uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,14 +2,38 @@
 #include <stdlib.h>
 #include "main.h"
 
-unsigned int binary_to_uint(const char *binary);
+unsigned int binary_to_uint(const char *b);
+
 int _putchar(char c);
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: string containing the binary number.
+ *
+ * Return: the converted number, or 0 if there is one or more chars in the string
+ * b that is not 0 or 1, or if b is NULL.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+    unsigned int dec_val = 0;
+
+    if (!b)
+        return 0;
+
+    for (int i = 0; b[i] != '\0'; i++)
+    {
+        if (b[i] < '0' || b[i] > '1')
+            return 0;
+        dec_val = 2 * dec_val + (b[i] - '0');
+    }
+
+    return dec_val;
+}
 
 int main(void)
 {
     const char *binary = "101010";  /* Example binary number */
     unsigned int decimal = 0;
-    unsigned int result; // Move the declaration here
     int i;
 
     if (binary == NULL)
@@ -23,7 +47,7 @@ int main(void)
         decimal = 2 * decimal + (binary[i] - '0');
     }
 
-    result = binary_to_uint(binary); // Assign the value here
+    unsigned int result = binary_to_uint(binary);
 
     if (result == 0)
         _putchar('0');
