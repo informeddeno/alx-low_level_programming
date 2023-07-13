b2d4ff6a91b22137946c76b6fb651dc947da372f
informeddeno <informeddeno@gmail.com>
2023-07-13 10:00:41 -0700

    headerfile

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,5 +9,6 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
+int main(int argc, char *argv[]);
+void exit_error(int exit_code, char *message);
 #endif
