7edc108a0e97c6dc080a381bf41863947a2afd47
informeddeno <informeddeno@gmail.com>
2023-07-16 09:02:06 -0700

    copy file to file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,63 +1,106 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include "main.h"
-#define BUFFER_SIZE 1024
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * exit_error - Prints an error message and exits with the specified code
- * @exit_code: The exit code to use
- * @message: The error message to print
+ * print_error- Prints the error message to stderr.
+ * @msg: The error message to be printed.
  */
-void exit_error(int exit_code, char *message)
+void print_error(const char *msg);
+
+/**
+ * copy_file- Copies the content of one file to another file.
+ * @file_from: The source file path.
+ * @file_to: The destination file path.
+ */
+int copy_file(const char *file_from, const char *file_to);
+void print_error(const char *msg)
 {
-	dprintf(STDERR_FILENO, "%s\n", message);
-	exit(exit_code);
+	write(STDERR_FILENO, msg, strlen(msg));
 }
 
-/**
- * main - Entry point of the program
- * @argc: The number of command-line arguments
- * @argv: An array containing the command-line arguments
- *
- * Return: 0 on success, otherwise an error code
- */
-int main(int argc, char *argv[])
+int copy_file(const char *file_from, const char *file_to)
 {
-	int src_fd, dest_fd, read_bytes, write_bytes;
-	char *buffer;
+	char buf[1024];
+	int fd_from, fd_to, ret;
+	ssize_t rd;
 
-	if (argc != 3)
-		exit_error(97, "Usage: cp file_from file_to");
-
-	src_fd = open(argv[1], O_RDONLY);
-	if (src_fd == -1)
-		exit_error(98, "Error: Can't read from source file");
-
-	dest_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (dest_fd == -1)
-		exit_error(99, "Error: Can't write to destination file");
-
-	buffer = malloc(BUFFER_SIZE);
-	if (buffer == NULL)
-		exit_error(99, "Error: Unable to allocate memory");
-
-	while ((read_bytes = read(src_fd, buffer, BUFFER_SIZE)) > 0)
+	fd_from = open(file_from, O_RDONLY);
+	if (fd_from == -1)
 	{
-		write_bytes = write(dest_fd, buffer, read_bytes);
-		if (write_bytes != read_bytes || write_bytes == -1)
-			exit_error(99, "Error: Can't write to destination file");
+		return (98); /* Error: Can't read from file */
 	}
 
-	if (read_bytes == -1)
-		exit_error(98, "Error: Can't read from source file");
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		close(fd_from);
+		return (99); /* Error: Can't write to file */
+	}
 
-	free(buffer);
+	while ((rd = read(fd_from, buf, sizeof(buf))) > 0)
+	{
+		ret = write(fd_to, buf, rd);
+		if (ret == -1 || ret != rd)
+		{
+			close(fd_from);
+			close(fd_to);
+			return (99); /* Error: Can't write to file */
+		}
+	}
 
-	if (close(src_fd) == -1)
-		exit_error(100, "Error: Can't close source file descriptor");
+	if (rd == -1)
+	{
+		close(fd_from);
+		close(fd_to);
+		return (98); /* Error: Can't read from file */
+	}
 
-	if (close(dest_fd) == -1)
-		exit_error(100, "Error: Can't close destination file descriptor");
+	ret = close(fd_from);
+	if (ret == -1)
+	{
+		return (100); /* Error: Can't close file */
+	}
+
+	ret = close(fd_to);
+	if (ret == -1)
+	{
+		return (100); /* Error: Can't close file */
+	}
+
+	return (0);
+}
+
+int main(int argc, char *argv[])
+{
+  int result;
+
+  if (argc != 3)
+	{
+		print_error("Usage: cp file_from file_to\n");
+		return (97); /* Error: Invalid number of arguments */
+	}
+
+	result = copy_file(argv[1], argv[2]);
+
+	if (result != 0)
+	{
+		if (result == 98)
+		{
+			print_error("Error: Can't read from file\n");
+		}
+		else if (result == 99)
+		{
+			print_error("Error: Can't write to file\n");
+		}
+		else if (result == 100)
+		{
+			print_error("Error: Can't close file\n");
+		}
+		return (result);
+	}
 
 	return (0);
 }
