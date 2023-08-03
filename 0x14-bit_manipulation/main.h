a7f4b51112979d96b0976d9efd6cc9dd6bcfcbf7
informeddeno <informeddeno@gmail.com>
2023-08-03 16:54:01 -0700

    bit manipulation

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -11,7 +11,7 @@ int copy_file(const char *source_file, const char *dest_file);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-int main(int argc, char *argv[]);
+int main();
 void exit_error(int exit_code, char *message);
 
 #include <unistd.h>
