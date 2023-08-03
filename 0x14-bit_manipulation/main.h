7b58c591b2778085089f7187292bd2e375487ee9
informeddeno <informeddeno@gmail.com>
2023-08-03 16:51:38 -0700

    bit manipulation

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,7 +6,6 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-int _putchar(char c);
 void print_error(const char *message);
 int copy_file(const char *source_file, const char *dest_file);
 ssize_t read_textfile(const char *filename, size_t letters);
@@ -14,4 +13,19 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int main(int argc, char *argv[]);
 void exit_error(int exit_code, char *message);
+
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
 #endif
