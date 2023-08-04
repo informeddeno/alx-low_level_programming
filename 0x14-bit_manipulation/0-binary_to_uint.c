5311fe4e33a9128d4216fa49dd98859cdef55146
informeddeno <informeddeno@gmail.com>
2023-08-04 02:07:52 -0700

    putchar

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,6 +3,20 @@
 #include <unistd.h>
 #include "main.h"
 
+#include <unistd.h>
+
+/**                                                                    
+ *_putchar - writes the character c to stdout                          
+ * @c: The character to print                                          
+ *                                                                     
+ *Return: On success 1.                                                
+ * On error, -1 is returned, and errno is set appropriately.           
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
+
 unsigned int binary_to_uint(const char *b)
 {
 unsigned int decimal = 0;
